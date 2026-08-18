0e094ac07e49e5835562b0875564914780d9fc7b
sahasra sri <alavalapatisahasrasrii@gmail.com>
2026-08-18 09:51:30 +0530

    Time: 3 ms (57.71%) | Memory: 44 MB (96.43%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -4,17 +4,19 @@ class Solution {
         int i = 0;
         int j = lo.length()-1;
        while(i<=j){
-        while (i<j && !Character.isLetterOrDigit(lo.charAt(i))){
+        if (i<j && !Character.isLetterOrDigit(lo.charAt(i))){
             i++;
         }
-        while(i<j && !Character.isLetterOrDigit(lo.charAt(j))){
+        else if(i<j && !Character.isLetterOrDigit(lo.charAt(j))){
             j--;
         }
-        if(lo.charAt(i) != lo.charAt(j)){
-            System.out.println(lo.charAt(i)+" "+lo.charAt(j));
+        else{
+            if(lo.charAt(i) != lo.charAt(j)){
+           // System.out.println(lo.charAt(i)+" "+lo.charAt(j));
             return false;
         }
         i++;j--;
+       }
        }
         return true;
     }
